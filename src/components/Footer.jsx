46a3c14cf1400c07384b81df7dcee9ee654d39bd
sreeharisreehari <kisreehari52@gmail.com>
2024-08-22 14:13:47 +0530

    img deletion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -76,74 +76,12 @@ function Footer() {
                                 </div>
                             </div>
                         </div>
-                        {/* <div class="col-xl-4 col-lg-4 col-sm-6">
-                            <div class="footer-widget mb-30">
-                                <div class="f-widget-title">
-                                    <h2>Work Gallery</h2>
-                                </div>
-                                <div class="f-insta">
-                                    <ul>
-                                        <li><a class="popup-image" href="img/instagram/f-galler-01.png"><img src="img/instagram/f-galler-01.png" alt="img"/></a></li>
-                                        <li><a class="popup-image" href="img/instagram/f-galler-02.png"><img src="img/instagram/f-galler-02.png" alt="img"/></a></li>
-                                        <li><a class="popup-image" href="img/instagram/f-galler-03.png"><img src="img/instagram/f-galler-03.png" alt="img"/></a></li>
-                                        <li><a class="popup-image" href="img/instagram/f-galler-04.png"><img src="img/instagram/f-galler-04.png" alt="img"/></a></li>
-                                        <li><a class="popup-image" href="img/instagram/f-galler-05.png"><img src="img/instagram/f-galler-05.png" alt="img"/></a></li>
-                                        <li><a class="popup-image" href="img/instagram/f-galler-06.png"><img src="img/instagram/f-galler-06.png" alt="img"/></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div> */}
+                       
                         
                     </div>
                 </div>
             </div>
-            {/* <div class="copyright-wrap">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-lg-4">
-                        <img  className='me-5' style={{width:'70px',marginLeft:'-40px'}} src="img/icon/Mukundiconog.png" alt="logo"/>
-                          
-                        </div>
-                        <div class="col-lg-4">          
-                            <div class="copy-text text-center">
-                                 Copyright &copy; 2024 Mukund Machine Tools. All rights reserved.       
-                            </div>
-                                
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="footer-social">                                    
-                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                    <a href="#"><i class="fab fa-instagram"></i></a>
-                                    <a href="#"><i class="fab fa-twitter"></i></a>
-                                </div>    
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-{/* 
-<div class="copyright-wrap">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-lg-4">
-                        <img  className='me-5' style={{width:'70px',marginLeft:'-40px'}} src="img/icon/Mukundiconog.png" alt="logo"/>
-                          
-                        </div>
-                        <div class="col-lg-4">          
-                            <div class="copy-text text-center">
-                                 Copyright &copy; 2024 Mukund Machine Tools. All rights reserved.       
-                            </div>
-                                
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="footer-social">                                    
-                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                    <a href="#"><i class="fab fa-instagram"></i></a>
-                                    <a href="#"><i class="fab fa-twitter"></i></a>
-                                </div>    
-                        </div>
-                    </div>
-                </div>
-            </div> */}
+           
         </footer>
 
 
