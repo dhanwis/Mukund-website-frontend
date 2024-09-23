6e57cf190c6c82a3deb7ea278902fd875379446c
sreeharisreehari <kisreehari52@gmail.com>
2024-09-23 12:07:32 +0530

    prodcut page added to center

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -49,49 +49,80 @@ function Products() {
 
     console.log(getProduct);
   return (
-    <div  >
-        <div data-aos="zoom-out-down" className='container'>
-            <br />
-            <br />
-            <br /><br />
-            <h1 style={{color:'rgb(0, 23, 60)'}} className='g1 text-center '>Product Gallery</h1>
-            <br />
-            <br />
-
-
-
-
-
-
-<div className='row'>
-
+    <div>
+    <div data-aos="zoom-out-down" className="container">
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1 style={{ color: "rgb(0, 23, 60)" }} className="g1 text-center">
+        Product Gallery
+      </h1>
+      <br />
+      <br />
   
- {
-  getProduct && getProduct.length> 0 ?
-  getProduct.map((item)=>(
-   
-
- 
- <div className='col-12 col-md-6 col-lg-4 mb-4'>
-  <Card style={{ width: '18rem'  
- }}>
-<Link to={`/productdetail/${item._id}`}>
-    <Card.Img style={{ height: '200px' }} variant="top" src={`${BASE_URL}/uploads/${item.image[0]}`} />
+      <div className="row">
+        {getProduct && getProduct.length > 0 ? (
+          getProduct.map((item) => (
+            <div className="col-12 col-sm-6 col-lg-4 mb-4">
+              <div className="d-flex justify-content-center justify-content-sm-start">
+                <Card style={{ width: "18rem" }}>
+                  <Link to={`/productdetail/${item._id}`}>
+                    <Card.Img
+                      style={{ height: "200px" }}
+                      variant="top"
+                      src={`${BASE_URL}/uploads/${item.image[0]}`}
+                    />
+                  </Link>
+                  <Card.Body>
+                    <Card.Title style={{ width: "15rem", height: "2rem" }}>
+                      {item.productname}
+                    </Card.Title>
+                    <Card.Text
+                      style={{ width: "15rem", height: "13rem", whiteSpace: "pre-wrap" }}
+                    >
+                      {item.description}
+                    </Card.Text>
+                    <Button
+                      href="https://wa.me/7259126513"
+                      style={{
+                        backgroundColor: "rgb(210, 33, 39)",
+                        borderColor: "rgb(210, 33, 39)",
+                      }}
+                    >
+                      Get a Quote <i className="fa-solid fa-arrow-right"></i>
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            </div>
+          ))
+        ) : (
+          <center>
+            <img
+              className="mb-5"
+              style={{ width: "45%" }}
+              src="img/icon/no-data2.1.gif"
+              alt="No products found"
+            />
+            <h4 className="mb-5" style={{ color: "#d22127" }}>
+              No products found!
+            </h4>
+          </center>
+        )}
+      </div>
   
-</Link>       
-      <Card.Body>
-        <Card.Title  style={{width:'15rem',height:'2rem'}}>{item.productname}</Card.Title>
-        <Card.Text style={{width:'15rem',height:'13rem', whiteSpace: 'pre-wrap'}} >
-                              {item.description}   
-                            </Card.Text>
-        <Button href='https://wa.me/7259126513' style={{backgroundColor:'rgb(210, 33, 39)',borderColor:'rgb(210, 33, 39)'}}>Get a Quote <i class="fa-solid fa-arrow-right"></i></Button>
-      </Card.Body>
-    </Card>
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
   </div>
-   ))
- : <center>  <img  className='mb-5 ' style={{width:'45%'}} src="img/icon/no-data2.1.gif" alt="Description of the image" /><h4 className='mb-5' style={{color:'#d22127'}}>No products found!</h4></center>
   
+  )
 }
+
+export default Products
 
 {/* <div className='col-12 col-md-6 col-lg-4 mb-4'>
   <Card style={{ width: '18rem'  
@@ -158,21 +189,3 @@ function Products() {
       </Card.Body>
     </Card>
   </div> */}
- 
-</div>
-
-    <br />
-    <br />
-<br />
-<br />
-
-
-
-        </div>
-
-
-    </div>
-  )
-}
-
-export default Products
